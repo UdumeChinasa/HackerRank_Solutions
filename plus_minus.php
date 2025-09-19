21743a7ce0b27bc8ef4db078365caa7f7f6d357b
Chinasa <chinasaudume42@gmail.com>
2025-09-19 15:13:43 +0100

    Add HackerRank Solution 5

---
+++ plus_minus.php
@@ -0,0 +1,46 @@
+<?php
+
+/*
+ * Complete the 'plusMinus' function below.
+ *
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+function plusMinus($arr) {
+    // Write your code here
+    
+    
+    $n = count($arr);
+
+    $positive = 0;
+    $negative = 0;
+    $zerovalues =0;
+    
+    for($i=0; $i < $n; ++$i){
+        if($arr[$i] > 0){
+           $positive++;
+        }
+         elseif($arr[$i] < 0){
+            $negative++;
+         }
+         else{
+            $zerovalues++;
+         }
+    
+    }
+          printf("%.6f\n", $positive/$n);
+    printf("%.6f\n", $negative/$n);
+    printf("%.6f\n", $zerovalues/$n);
+    
+}
+$n = intval(trim(fgets(STDIN)));
+
+$arr_temp = rtrim(fgets(STDIN));
+
+$arr = array_map('intval', preg_split('/ /', $arr_temp, -1, PREG_SPLIT_NO_EMPTY));
+    
+   
+plusMinus($arr);
+
+
+
